08fd10d08291e8b26bab24fc68d9aa6da326951c
sama-004 <70210929+Sama-004@users.noreply.github.com>
2024-07-01 23:56:36 +0530

    mistral && hardcoded prompt

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,16 +1,27 @@
 import { openai } from "@ai-sdk/openai";
+import { mistral } from "@ai-sdk/mistral";
 import { streamText } from "ai";
+import { generateText } from "ai";
+import { NextResponse } from "next/server";
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
-  const { messages } = await req.json();
+  //   const { messages } = await req.json();
+  const body = await req.json();
 
-  const result = await streamText({
-    model: openai("gpt-3.5-turbo"),
-    messages,
+  const message = body.messages;
+
+  console.log("Recieved message:", message);
+
+  const result = await generateText({
+    model: mistral("open-mistral-7b"),
+    prompt: "Tell me what is arch linux",
   });
 
-  return result.toAIStreamResponse();
+  console.log(result.text);
+  return NextResponse.json(result.text);
+  //   return NextResponse.json(result.text);
+  //   return NextResponse.json(text);
 }
